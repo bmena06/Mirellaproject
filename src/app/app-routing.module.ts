93e3edc821fc9ecf6aadd0267d6a0a3447f79d23
OfeLG <ofelialondonog@gmail.com>
2023-05-28 12:37:28 -0500

    borrando unas carpetas, por conflictos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { CardComponentComponent } from './card-component/card-component.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent},
-  { path: 'carrito', component: CardComponentComponent},
 
 ];
 
