0a5f3312822da247c173cdfe61308792b489a06e
OfeLG <ofelialondonog@gmail.com>
2023-05-28 12:25:12 -0500

    cambio en la estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { CardComponentComponent } from './card-component/card-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     HomePageComponent,
-    CardComponentComponent
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
